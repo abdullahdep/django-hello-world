a58b1d00da3702de81901d6577e2b99e68afd90e
abdullahdep <abdullah08dep@gmail.com>
2025-06-06 01:30:14 +0500

    jjk

--- api/settings.py
+++ api/settings.py
@@ -169,6 +169,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files (uploads)
 MEDIA_URL = '/media/'
